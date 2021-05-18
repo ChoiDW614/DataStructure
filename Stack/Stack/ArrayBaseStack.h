6e56be6f2247a173395826a5d60fc15ed44f5662
ChoiDW614 <81404753+ChoiDW614@users.noreply.github.com>
2021-05-18 19:20:44 +0900

    Update ArrayBaseStack.h

--- Stack/Stack/ArrayBaseStack.h
+++ Stack/Stack/ArrayBaseStack.h
@@ -1,11 +1,26 @@
 #ifndef __ARRAY_BASE_STACK_H__
 #define __ARRAY_BASE_STACK_H__
 
-#include <iostream>
+constexpr int TRUE      = 1;
+constexpr int FALSE     = 0;
+constexpr int STACK_LEN = 100;
 
+typedef int Data;
 
+typedef class arrayStack
+{
+public:
+    Data stackArr[STACK_LEN];
+    int topIndex;
+}ArrayStack;
 
-void StackInit(Stack * pstack);
+typedef ArrayStack Stack;
 
+void StackInit(Stack * pstack);             // initialize stack
+int SIsEmpty(Stack * pstack);               // check if the stack is empty
+
+void SPush(Stack * pstack, Data data);      // stack Push opertaion
+Data SPop(Stack * pstack);                  // stack Pop opertaion
+Data SPeek(Stack * pstack);                 // stack Peek opertaion
 
 #endif
